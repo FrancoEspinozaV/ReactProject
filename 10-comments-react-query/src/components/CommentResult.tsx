a22af08d987629f81a2b7003bc5f69ed85041123
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-01-19 20:21:17 -0300

    add delete button users and loading

--- 10-comments-react-query/src/components/CommentResult.tsx
+++ 10-comments-react-query/src/components/CommentResult.tsx
@@ -1,13 +1,21 @@
 import { Button, Card, Text, Title } from "@tremor/react";
+import { useComments } from "../hooks/useComments";
 import { type CommentWithId } from "../interface.d";
 
 export function CommentResult({ data }: { data?: CommentWithId[] }) {
+	const { deletePost, loading } = useComments();
 	return data?.map((comment) => (
 		<ul key={comment.id}>
 			<li>
 				<Card className="rounded-md">
 					<Title className="text-2xl flex justify-between">
-						{comment.title} <Button>Eliminar</Button>
+						{comment.title}{" "}
+						<Button
+							disabled={loading}
+							onClick={() => deletePost({ id: comment.id })}
+						>
+							Eliminar
+						</Button>
 					</Title>
 					<Text>{comment.message}</Text>
 				</Card>
