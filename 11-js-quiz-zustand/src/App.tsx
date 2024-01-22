4cdef012a84c8c0b027f93b50587ab61c88b715a
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-01-21 21:01:29 -0300

    add footers and change position icon

--- 11-js-quiz-zustand/src/App.tsx
+++ 11-js-quiz-zustand/src/App.tsx
@@ -2,26 +2,28 @@ import { Container, Stack, Typography } from "@mui/material";
 import "./App.css";
 import { JsIcon } from "./Icons/JsIcon";
 import { ButtonStart } from "./components/ButtonStart";
+import { Footer } from "./components/Footer";
 import { Game } from "./components/Game";
 import { useQuestionsStore } from "./services/question";
 function App() {
 	const questions = useQuestionsStore((state) => state.questions);
 	return (
 		<main>
-			<Container maxWidth="sm">
+			<Container maxWidth="sm" fixed sx={{ height: "100vh" }}>
 				<Stack
 					direction="row"
 					gap={2}
 					alignItems="center"
 					justifyContent="center"
 				>
-					<JsIcon />
 					<Typography variant="h2" component="h1">
+						<JsIcon />
 						Quizz JavaScript
 					</Typography>
 				</Stack>
 				{questions.length === 0 && <ButtonStart />}
 				{questions.length > 0 && <Game />}
+				{questions.length > 0 && <Footer />}
 			</Container>
 		</main>
 	);
