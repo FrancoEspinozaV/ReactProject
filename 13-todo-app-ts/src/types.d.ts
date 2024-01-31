5d621a57e3b0d73137365681d55d11d680e9eafb
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-01-31 00:01:23 -0300

    refactor types

--- 13-todo-app-ts/src/types.d.ts
+++ 13-todo-app-ts/src/types.d.ts
@@ -1,7 +1,11 @@
+export type TodoId = string
+export type TodoTitle = string
+export type TodoCompleted = boolean
+
 export interface Todo {
-  id: string
-  title: string
-  completed: boolean
+  id: TodoId
+  title: TodoTitle
+  completed: TodoCompleted
 }
 
 export type ListOfTodo = Todo[]
